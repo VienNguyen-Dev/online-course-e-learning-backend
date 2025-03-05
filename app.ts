58f46edc94e2a729a0e21b1074b5f8a264c82c9a
VienNguyen <chivien107@gmail.com>
2025-03-05 09:22:28 +0700

    change

--- app.ts
+++ app.ts
@@ -13,23 +13,14 @@ import "./config/passport";
 import homeRouter from "./routes/home.route";
 
 const app = express();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://online-course-e-learning-frontend.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
-app.use(
-  cors({
-    origin: "https://online-course-e-learning-frontend.vercel.app",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
