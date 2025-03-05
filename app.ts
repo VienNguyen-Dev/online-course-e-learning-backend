f08c0cfe1839adae5698860993cf0a7cc3d8e32b
VienNguyen <chivien107@gmail.com>
2025-03-05 09:08:00 +0700

    config header

--- app.ts
+++ app.ts
@@ -13,9 +13,18 @@ import "./config/passport";
 import homeRouter from "./routes/home.route";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://online-course-e-learning-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(
-  // cors()
   cors({
     origin: "https://online-course-e-learning-frontend.vercel.app",
     credentials: true,
